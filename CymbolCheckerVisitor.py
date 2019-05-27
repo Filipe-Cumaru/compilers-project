014becfa749e1cde0557538a0a52363787667044
Filipe Antonio Cumaru Silva Alves <facsa@ug2c31.windows.cin.ufpe.br>
2019-05-27 11:07:08 -0300

    Starting to implement visitor

--- CymbolCheckerVisitor.py
+++ CymbolCheckerVisitor.py
@@ -3,4 +3,6 @@ from autogen.CymbolParser import CymbolParser
 from autogen.CymbolVisitor import CymbolVisitor
 
 class CymbolCheckerVisitor (CymbolVisitor):
-    
+
+    def visitVarDecl(self, ctx:CymbolParser.VarDeclContext):
+        return self.visitChildren(ctx)
