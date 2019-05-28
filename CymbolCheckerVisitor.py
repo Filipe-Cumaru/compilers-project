0b3d79d1b330c98e9bf090a194fa19a9f519948b
Filipe-Cumaru <facsa@cin.ufpe.br>
2019-05-27 21:19:47 -0300

    Fixed tab and spaces inconsistence

--- CymbolCheckerVisitor.py
+++ CymbolCheckerVisitor.py
@@ -8,41 +8,43 @@ class Type:
 	BOOLEAN = "boolean"
 
 class CymbolCheckerVisitor (CymbolVisitor):
-    program = ""
+	program = ""
 
-    def visitVarDecl(self, ctx:CymbolParser.VarDeclContext):
-        var_name = ctx.ID().getText()
+	def visitVarDecl(self, ctx:CymbolParser.VarDeclContext):
+		var_name = ctx.ID().getText()
 		var_type = ctx.tyype().getText()
 
         # Add line with variable allocation.
-        self.program = "%{0} = alloca ".format(var_name)
-        if var_type == Type.INT:
-            self.program += "i32, align 4\n"
-        elif var_type == Type.FLOAT:
-            self.program += "float, align 4\n"
-        elif var_type == Type.BOOLEAN:
-            self.program += "i8, align 4\n"
+		self.program = "%{0} = alloca ".format(var_name)
+		if var_type == Type.INT:
+			self.program += "i32, align 4\n"
+		elif var_type == Type.FLOAT:
+			self.program += "float, align 4\n"
+		elif var_type == Type.BOOLEAN:
+			self.program += "i8, align 4\n"
 
-        if ctx.expr() != None:
-            ctx.expr().accept(self)
+		if ctx.expr() != None:
+			ctx.expr().accept(self)
 
-    def visitFuncDecl(self, ctx:CymbolParser.FuncDeclContext):
-        func_name = ctx.ID().getText()
-        func_ret_type = ctx.ID().getText()
+		print(self.program)
+
+	def visitFuncDecl(self, ctx:CymbolParser.FuncDeclContext):
+		func_name = ctx.ID().getText()
+		func_ret_type = ctx.ID().getText()
 
         # Add function declaration and return type.
-        self.program += "define "
-        if func_ret_type == Type.INT:
-            self.program += "i32 "
-        elif func_ret_type == Type.FLOAT:
-            self.program += "float "
-        elif func_ret_type == Type.BOOLEAN:
-            self.program += "i8 "
+		self.program += "define "
+		if func_ret_type == Type.INT:
+			self.program += "i32 "
+		elif func_ret_type == Type.FLOAT:
+			self.program += "float "
+		elif func_ret_type == Type.BOOLEAN:
+			self.program += "i8 "
 
-        self.program += "@{0}(".format(func_name)
+		self.program += "@{0}(".format(func_name)
         # NOTE: Not sure if this is right. Still needs to be tested.
-        if ctx.paramTypeList() != None:
-            ctx.paramTypeList().accept(self)
-        self.program += ") {\n\t"
-        ctx.block().accept(self)
-        self.program += "}"
+		if ctx.paramTypeList() != None:
+			ctx.paramTypeList().accept(self)
+		self.program += ") {\n\t"
+		ctx.block().accept(self)
+		self.program += "}"
